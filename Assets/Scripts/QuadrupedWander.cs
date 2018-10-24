f83dc00584364c77cfc8750b2591fe95842ba435
DESKTOP-CU5HT44\Babak <bkfiroozi@gmail.com>
2018-10-24 14:36:29 -0700

    first commit

---
+++ Assets/Scripts/QuadrupedWander.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadrupedWander : MonoBehaviour
+{
+	WanderState _currentState;
+
+	[SerializeField] Animator _animator = null;
+	[SerializeField] float _maxSpeed = .1f;
+
+	[SerializeField] float _walkAnimSpeedTweaker = 5;
+
+	const float Move_Force = 20;
+
+	float _needForce;
+
+	Transform _tr;
+	Rigidbody _rigidbody;
+
+	public Animator Animator{ get { return _animator; } }
+	public Rigidbody Rigidbody{ get { return _rigidbody; } }
+	public float MaxSpeed{ get { return _maxSpeed; } }
+	public float WalkAnimSpeedTweaker{ get { return _walkAnimSpeedTweaker; } }
+	public float NeedForce{ get { return _needForce; } }
+	public Transform Tr{ get { return _tr; } }
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		_tr = transform;
+		_rigidbody = GetComponent<Rigidbody> ();
+
+		_needForce = Move_Force * (_maxSpeed * 10);
+
+		CurrentState = new WanderState_Move (this);
+	}
+
+	void FixedUpdate()
+	{
+		_currentState.OnStateRunFixed ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_currentState.OnStateRun ();
+	}
+
+
+	public WanderState CurrentState
+	{
+		get	{ return _currentState; }
+		set
+		{
+			var state = value;
+			if (_currentState != null)
+				_currentState.OnStateExit ();
+			_currentState = state;
+			_currentState.OnStateEnter ();
+		}
+	}
+}
+
---
+++ Assets/Scripts/WanderStates.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WanderState
+{
+	protected QuadrupedWander _quadruped;
+
+	public WanderState(QuadrupedWander qaudruped)
+	{
+		_quadruped = qaudruped;
+	}
+
+	public abstract void OnStateRun();
+	public abstract void OnStateRunFixed();
+
+	public virtual void OnStateEnter()
+	{
+	}
+	public virtual void OnStateExit()
+	{
+	}
+}
+
+/// <summary>
+/// Idle state implementation for wander
+/// </summary>
+public class WanderState_Idle : WanderState
+{
+	public WanderState_Idle(QuadrupedWander quadruped) : base(quadruped)
+	{
+	}
+
+	public override void OnStateRun ()
+	{
+	}
+
+	public override void OnStateRunFixed ()
+	{
+	}
+
+	public override void OnStateEnter ()
+	{
+		base.OnStateEnter ();
+		_quadruped.Animator.SetTrigger ("idle");
+	}
+
+	public override void OnStateExit ()
+	{
+		base.OnStateExit ();
+	}
+}
+
+/// <summary>
+/// Move state implementation for wander
+/// </summary>
+public class WanderState_Move : WanderState
+{
+	public WanderState_Move(QuadrupedWander quadruped) : base(quadruped)
+	{
+	}
+
+	public override void OnStateRun ()
+	{
+		Vector3 vel = _quadruped.Rigidbody.velocity;
+		_quadruped.Animator.SetFloat ("walkSpeedMult", vel.magnitude * _quadruped.WalkAnimSpeedTweaker);
+	}
+
+	public override void OnStateRunFixed ()
+	{
+		Vector3 moveDir = _quadruped.Tr.forward;
+		var _rigidbody = _quadruped.Rigidbody;
+		float _maxSpeed = _quadruped.MaxSpeed;
+
+		Vector3 vel = _rigidbody.velocity;
+		_rigidbody.velocity = Vector3.zero;
+
+		if(vel.magnitude < _maxSpeed)
+		{
+			float force = _quadruped.NeedForce;
+			Vector3 forceVec = moveDir * force * Time.fixedDeltaTime;
+			_rigidbody.AddForce (forceVec, ForceMode.Impulse);
+		}
+	}
+
+	public override void OnStateEnter ()
+	{
+		base.OnStateEnter ();
+		_quadruped.Animator.SetTrigger ("walk");
+	}
+
+	public override void OnStateExit ()
+	{
+		base.OnStateExit ();
+	}
+}
